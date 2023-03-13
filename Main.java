ae34ad105499415545d003b92f7338280769b4a2
omkarmahasuar99 <126793930+omkarmahasuar99@users.noreply.github.com>
2023-03-13 20:36:48 +0530

    greatest between 2 numbers

---
+++ Main.java
@@ -0,0 +1,12 @@
+class greatest_between_2_numbers {
+    public static void main(String[] args) {
+        int a=5;
+        int b=32;
+        if (a>b) {
+            System.out.println(a + " is the greater number");
+        }
+        else System.out.println(b + " is the greater number");
+
+
+    }
+}
