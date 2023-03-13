62dc982e9387642e1766935ceaf0e13e63c83e09
omkarmahasuar99 <126793930+omkarmahasuar99@users.noreply.github.com>
2023-03-13 21:03:59 +0530

    negative or positive number

--- Main.java
+++ Main.java
@@ -1,19 +1,11 @@
-class Next_Number
+class Negative_or_Positive
 {
     public static void main(String[] args)
     {
-        int a=71;
-        int b=a+1;
-        System.out.println(b+" is the next number");
-    }
+        int a = -89;
+        if (a>0)
+            System.out.println(a+" is a positive number");
+        else System.out.println(a+" is a negative number");
 
-}
-class Previous_Number
-{
-    public static void main(String[] args)
-    {
-        int a=84;
-        int b=a-1;
-        System.out.println(b+" is the previous number");
     }
 }
